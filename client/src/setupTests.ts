e8b99ba7b3d22778bcdc6a8955604eb1e3a416c1
khalilabdalmajeed <kamkmgamer@gmail.com>
2025-08-12 00:30:31 +0300

    Update setupTests.ts

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,5 +1,4 @@
-/// <reference types="@testing-library/jest-dom" />
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom/extend-expect';
 import { TextEncoder, TextDecoder } from 'util';
 
 Object.assign(global, { TextDecoder, TextEncoder });
